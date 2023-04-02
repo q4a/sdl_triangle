4cdda783cd5381c8ed728753e8be5887dd271e35
Aleksey Komarov <q4arus@ya.ru>
2023-04-02 08:43:34 +0300

    nine: rename Device to device and add test_table_fog_zw() from Nine

--- sdl_d3d9_triangle/src/sdl_d3d9_triangle.cpp
+++ sdl_d3d9_triangle/src/sdl_d3d9_triangle.cpp
@@ -6,7 +6,7 @@
 
 // Globals
 
-IDirect3DDevice9* Device = 0;
+IDirect3DDevice9* device = 0;
 const int Width = 640;
 const int Height = 480;
 const int SECOND = 1000;
@@ -73,7 +73,7 @@ bool Setup()
 {
 	// Create the vertex buffer.
 
-	Device->CreateVertexBuffer(
+	device->CreateVertexBuffer(
 		3 * sizeof(Vertex), // size in bytes
 		D3DUSAGE_WRITEONLY, // flags
 		Vertex::FVF,        // vertex format
@@ -101,11 +101,11 @@ bool Setup()
 		(float)Width / (float)Height, // aspect ratio
 		1.0f,                         // near plane
 		1000.0f);                     // far plane
-	Device->SetTransform(D3DTS_PROJECTION, &proj);
+	device->SetTransform(D3DTS_PROJECTION, &proj);
 
 	// Set wireframe mode render state.
 
-	Device->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+	device->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
 
 	return true;
 }
@@ -117,23 +117,21 @@ void Cleanup()
 
 void ShowPrimitive()
 {
-	if (Device)
+	if (device)
 	{
-		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
-		Device->BeginScene();
+		device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+		device->BeginScene();
 
-		Device->SetStreamSource(0, Triangle, 0, sizeof(Vertex));
-		Device->SetFVF(Vertex::FVF);
+		device->SetStreamSource(0, Triangle, 0, sizeof(Vertex));
+		device->SetFVF(Vertex::FVF);
 
 		// Draw one triangle.
-		Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+		device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
 
-		Device->EndScene();
-		Device->Present(0, 0, 0, 0);
+		device->EndScene();
+		device->Present(0, 0, 0, 0);
 	}
 }
-
-
 
 // init ... The init function, it calls the SDL init function.
 int initSDL() {
@@ -163,6 +161,261 @@ SDL_Window* createWindowContext(std::string title) {
 	return Window;
 }
 
+
+int __cdecl fprintf2(FILE* stream, const char* format, ...)
+{
+	char str[1024];
+
+	va_list argptr;
+	va_start(argptr, format);
+	int ret = vsnprintf(str, sizeof(str), format, argptr);
+	va_end(argptr);
+
+	OutputDebugStringA(str);
+
+	return ret;
+}
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
+
+#define ok(c, ...) {{if (!(c)) fprintf2(stdout, "fail %s ", __func__); else fprintf2(stdout, "succ %s ", __func__); fprintf2(stdout, __VA_ARGS__);}}
+#define skip(...) {fprintf2(stdout, "skip "); fprintf2(stdout, __VA_ARGS__);}
+#define trace(...) {fprintf2(stdout, "trace ");fprintf2(stdout, __VA_ARGS__);}
+
+struct vec4
+{
+	float x, y, z, w;
+};
+
+struct surface_readback
+{
+	IDirect3DSurface9* surface;
+	D3DLOCKED_RECT locked_rect;
+};
+
+static void get_rt_readback(IDirect3DSurface9* surface, struct surface_readback* rb)
+{
+	//IDirect3DDevice9* device;
+	D3DSURFACE_DESC desc;
+	HRESULT hr;
+
+	memset(rb, 0, sizeof(*rb));
+	hr = IDirect3DSurface9_GetDevice(surface, &device);
+	ok(SUCCEEDED(hr), "Failed to get device, hr %#x.\n", hr);
+	hr = IDirect3DSurface9_GetDesc(surface, &desc);
+	ok(SUCCEEDED(hr), "Failed to get surface desc, hr %#x.\n", hr);
+	hr = IDirect3DDevice9_CreateOffscreenPlainSurface(device, desc.Width, desc.Height,
+		desc.Format, D3DPOOL_SYSTEMMEM, &rb->surface, NULL);
+	if (FAILED(hr) || !rb->surface)
+	{
+		trace("Can't create an offscreen plain surface to read the render target data, hr %#x.\n", hr);
+		goto error;
+	}
+
+	hr = IDirect3DDevice9_GetRenderTargetData(device, surface, rb->surface);
+	if (FAILED(hr))
+	{
+		trace("Can't read the render target data, hr %#x.\n", hr);
+		goto error;
+	}
+
+	hr = IDirect3DSurface9_LockRect(rb->surface, &rb->locked_rect, NULL, D3DLOCK_READONLY);
+	if (FAILED(hr))
+	{
+		trace("Can't lock the offscreen surface, hr %#x.\n", hr);
+		goto error;
+	}
+	IDirect3DDevice9_Release(device);
+
+	return;
+
+error:
+	if (rb->surface)
+		IDirect3DSurface9_Release(rb->surface);
+	rb->surface = NULL;
+	IDirect3DDevice9_Release(device);
+}
+
+static DWORD get_readback_color(struct surface_readback* rb, unsigned int x, unsigned int y)
+{
+	return rb->locked_rect.pBits
+		? ((DWORD*)rb->locked_rect.pBits)[y * rb->locked_rect.Pitch / sizeof(DWORD) + x] : 0xdeadbeef;
+}
+
+static void release_surface_readback(struct surface_readback* rb)
+{
+	HRESULT hr;
+
+	if (!rb->surface)
+		return;
+	if (rb->locked_rect.pBits && FAILED(hr = IDirect3DSurface9_UnlockRect(rb->surface)))
+		trace("Can't unlock the offscreen surface, hr %#x.\n", hr);
+	IDirect3DSurface9_Release(rb->surface);
+}
+
+static BOOL compare_uint(unsigned int x, unsigned int y, unsigned int max_diff)
+{
+	unsigned int diff = x > y ? x - y : y - x;
+
+	return diff <= max_diff;
+}
+
+static BOOL color_match(D3DCOLOR c1, D3DCOLOR c2, BYTE max_diff)
+{
+	return compare_uint(c1 & 0xff, c2 & 0xff, max_diff)
+		&& compare_uint((c1 >> 8) & 0xff, (c2 >> 8) & 0xff, max_diff)
+		&& compare_uint((c1 >> 16) & 0xff, (c2 >> 16) & 0xff, max_diff)
+		&& compare_uint((c1 >> 24) & 0xff, (c2 >> 24) & 0xff, max_diff);
+}
+
+static DWORD getPixelColor(IDirect3DDevice9* device, UINT x, UINT y)
+{
+	DWORD ret;
+	IDirect3DSurface9* rt;
+	struct surface_readback rb;
+	HRESULT hr;
+
+	hr = IDirect3DDevice9_GetRenderTarget(device, 0, &rt);
+	if (FAILED(hr))
+	{
+		trace("Can't get the render target, hr %#x.\n", hr);
+		return 0xdeadbeed;
+	}
+
+	get_rt_readback(rt, &rb);
+	/* Remove the X channel for now. DirectX and OpenGL have different ideas how to treat it apparently, and it isn't
+	 * really important for these tests
+	 */
+	ret = get_readback_color(&rb, x, y) & 0x00ffffff;
+	release_surface_readback(&rb);
+
+	IDirect3DSurface9_Release(rt);
+	return ret;
+}
+
+static void test_table_fog_zw(void)
+{
+	HRESULT hr;
+	//IDirect3DDevice9* device;
+	//IDirect3D9* d3d;
+	ULONG refcount;
+	HWND window;
+	D3DCOLOR color;
+	D3DCAPS9 caps;
+	static struct
+	{
+		struct vec4 position;
+		D3DCOLOR diffuse;
+	}
+	quad[] =
+	{
+		{{  0.0f,   0.0f, 0.0f, 0.0f}, 0xffff0000},
+		{{640.0f,   0.0f, 0.0f, 0.0f}, 0xffff0000},
+		{{  0.0f, 480.0f, 0.0f, 0.0f}, 0xffff0000},
+		{{640.0f, 480.0f, 0.0f, 0.0f}, 0xffff0000},
+	};
+	static const D3DMATRIX identity =
+	{ {{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	}} };
+	static const struct
+	{
+		float z, w;
+		D3DZBUFFERTYPE z_test;
+		D3DCOLOR color;
+	}
+	tests[] =
+	{
+		{0.7f,  0.0f, D3DZB_TRUE,  0x0000b200},
+		{0.7f,  0.0f, D3DZB_FALSE, 0x0000b200},
+		{0.7f,  0.3f, D3DZB_TRUE,  0x004cb200},
+		{0.7f,  0.3f, D3DZB_FALSE, 0x004cb200},
+		{0.7f,  3.0f, D3DZB_TRUE,  0x004cb200},
+		{0.7f,  3.0f, D3DZB_FALSE, 0x004cb200},
+		{0.3f,  0.0f, D3DZB_TRUE,  0x00004c00},
+		{0.3f,  0.0f, D3DZB_FALSE, 0x00004c00},
+	};
+	unsigned int i;
+
+	/*
+	window = create_window();
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);
+	ok(!!d3d, "Failed to create a D3D object.\n");
+
+	if (!(device = create_device(d3d, window, window, TRUE)))
+	{
+		skip("Failed to create a D3D device, skipping tests.\n");
+		IDirect3D9_Release(d3d);
+		DestroyWindow(window);
+		return;
+	}
+	*/
+
+	hr = IDirect3DDevice9_GetDeviceCaps(device, &caps);
+	ok(SUCCEEDED(hr), "Failed to get device caps, hr %#x.\n", hr);
+	if (!(caps.RasterCaps & D3DPRASTERCAPS_FOGTABLE))
+	{
+		skip("D3DPRASTERCAPS_FOGTABLE not supported, skipping POSITIONT table fog test.\n");
+		goto done;
+	}
+
+	hr = IDirect3DDevice9_SetRenderState(device, D3DRS_LIGHTING, FALSE);
+	ok(SUCCEEDED(hr), "Failed to set render state, hr %#x.\n", hr);
+	hr = IDirect3DDevice9_SetRenderState(device, D3DRS_FOGENABLE, TRUE);
+	ok(SUCCEEDED(hr), "Failed to set render state, hr %#x.\n", hr);
+	hr = IDirect3DDevice9_SetRenderState(device, D3DRS_FOGCOLOR, 0x0000ff00);
+	ok(SUCCEEDED(hr), "Failed to set render state, hr %#x.\n", hr);
+	hr = IDirect3DDevice9_SetRenderState(device, D3DRS_CLIPPING, FALSE);
+	ok(SUCCEEDED(hr), "SetRenderState failed, hr %#x.\n", hr);
+	/* Work around an AMD Windows driver bug. Needs a proj matrix applied redundantly. */
+	hr = IDirect3DDevice9_SetTransform(device, D3DTS_PROJECTION, &identity);
+	ok(SUCCEEDED(hr), "Failed to set projection transform, hr %#x.\n", hr);
+	hr = IDirect3DDevice9_SetRenderState(device, D3DRS_FOGTABLEMODE, D3DFOG_LINEAR);
+	ok(SUCCEEDED(hr), "Failed to set render state, hr %#x.\n", hr);
+	hr = IDirect3DDevice9_SetFVF(device, D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
+	ok(SUCCEEDED(hr), "Failed to set fvf, hr %#x.\n", hr);
+
+	for (i = 0; i < ARRAY_SIZE(tests); ++i)
+	{
+		hr = IDirect3DDevice9_Clear(device, 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0x000000ff, 1.0f, 0);
+		ok(SUCCEEDED(hr), "Failed to clear, hr %#x.\n", hr);
+
+		quad[0].position.z = tests[i].z;
+		quad[1].position.z = tests[i].z;
+		quad[2].position.z = tests[i].z;
+		quad[3].position.z = tests[i].z;
+		quad[0].position.w = tests[i].w;
+		quad[1].position.w = tests[i].w;
+		quad[2].position.w = tests[i].w;
+		quad[3].position.w = tests[i].w;
+		hr = IDirect3DDevice9_SetRenderState(device, D3DRS_ZENABLE, tests[i].z_test);
+		ok(SUCCEEDED(hr), "Failed to set render state, hr %#x.\n", hr);
+
+		hr = IDirect3DDevice9_BeginScene(device);
+		ok(SUCCEEDED(hr), "Failed to begin scene, hr %#x.\n", hr);
+		hr = IDirect3DDevice9_DrawPrimitiveUP(device, D3DPT_TRIANGLESTRIP, 2, quad, sizeof(quad[0]));
+		ok(SUCCEEDED(hr), "Failed to draw, hr %#x.\n", hr);
+		hr = IDirect3DDevice9_EndScene(device);
+		ok(SUCCEEDED(hr), "Failed to end scene, hr %#x.\n", hr);
+
+		color = getPixelColor(device, 320, 240);
+		ok(color_match(color, tests[i].color, 2),
+			"Got unexpected color 0x%08x, expected 0x%08x, case %u.\n", color, tests[i].color, i);
+		hr = IDirect3DDevice9_Present(device, NULL, NULL, NULL, NULL);
+		ok(SUCCEEDED(hr), "Failed to present, hr %#x.\n", hr);
+	}
+
+done:
+	hr = 0;
+	//refcount = IDirect3DDevice9_Release(device);
+	//ok(!refcount, "Device has %u references left.\n", refcount);
+	//IDirect3D9_Release(d3d);
+	//DestroyWindow(window);
+}
+
 // main ... The main function, right now it just calls the initialization of SDL.
 int main(int argc, char* argv[]) {
 	//Calling the SDL init stuff.
@@ -172,7 +425,7 @@ int main(int argc, char* argv[]) {
 	SDL_Window* Window = createWindowContext("Hello World!");
 
 	if (!d3d::InitD3D(Window,
-		Width, Height, true, D3DDEVTYPE_HAL, &Device))
+		Width, Height, true, D3DDEVTYPE_HAL, &device))
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "InitD3D() - FAILED", nullptr);
 		return 0;
@@ -183,6 +436,7 @@ int main(int argc, char* argv[]) {
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Setup() - FAILED", nullptr);
 		return 0;
 	}
+	test_table_fog_zw();
 
 	bool running = true;
 	while (running)
@@ -202,7 +456,7 @@ int main(int argc, char* argv[]) {
 
 	//Cleaning up everything.
 	Cleanup();
-	Device->Release();
+	device->Release();
 	SDL_Quit();
 
 	return 0;
